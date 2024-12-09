6636d61553b1f7342bc1bb429827e4de151a9326
William Munoz <william@godeveloper.io>
2024-12-09 00:56:26 -0500

    add database configuration and docs

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Mail Subscriber
+
+## Database Migration
+
+### sqlx-cli install
+
+```shell
+cargo install --version="~0.7" sqlx-cli --no-default-features --features rustls,postgres
+```
+
+```shell
+sqlx --help
+```
+
+### Install Postgres Client
+
+```shell
+sudo pacman -S postgresql-libs
+```
+
+```shell
+psql --help
+```
+
+### Create Subscriptions Table
+
+```shell
+export DATABASE_URL=postgres://postgres:password@localhost:5432/newsletter
+sqlx migrate add create_subscriptions_table                                                                                                                                                  ✔ 
+Creating migrations/20241209054200_create_subscriptions_table.sql
+```
+
+Create the table definition in the file `migrations/{timestamp}_create_subscriptions_table.sql`:
+
+```sql
+CREATE TABLE subscriptions(
+    id uuid NOT NULL,
+    PRIMARY KEY (id),
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    subscribed_at timestamptz NOT NULL
+);
+```
+
+### Run the migration
+
+```shell
+SKIP_DOCKER=true ./scripts/init_db.sh
+```
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,23 +2,52 @@
 set -x
 set -eo pipefail
 
-# check if a custom user has been set, otherwise default to 'postgres'
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.7' sqlx-cli \
+--no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
+
 DB_USER="${POSTGRES_USER:=postgres}"
-# check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
-# check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
-# check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-# launch postgres using Docker
-docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  -d postgres \
-  postgres -N 1000
-  # ^ increased maximum number of connections for testing purposes
+# allow to skip Docker if a dockerized postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run \
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    -d postgres \
+    postgres -N 1000
+    # ^ increased maximum number of connections for testing purposes
+fi
+
+# keep pinging postgres until it is ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "postgres is up and running on port ${DB_PORT} - running migrations now!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
+sqlx migrate run
+
+>&2 echo "postgres has been migrated, ready to go!"
